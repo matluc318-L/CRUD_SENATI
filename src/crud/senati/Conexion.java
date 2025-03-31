6a83cda45924581126e995a14ffb87db65acabfe
Mattias <1584692@senati.pe>
2025-03-31 09:07:07 -0500

    Crear la clase conexion y su metodo

---
+++ src/crud/senati/Conexion.java
@@ -0,0 +1,21 @@
+package crud.senati;
+import java.sql.*;
+
+public class Conexion {
+    public Connection conectar(){
+        String url = "jdbc:mysql://localhost:3306/senati?zeroDateTimeBehavior=CONVERT_TO_NULL ";
+        String user = "root";
+        String password = "";
+        Connection cn = null;
+        
+        try{
+            cn = DriverManager.getConnection(url,user, password);
+            if (cn != null){
+                System.out.println("Conectado");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return cn;
+    }
+}
