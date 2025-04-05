0486689c50cd27eb0f32a6abdbd1183cdf1279df
matluc318-L <1584692@senati.pe>
2025-04-05 17:18:54 -0500

    conexion1

---
+++ src/Entregable/conexion.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entregable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author math3
+ */
+public class conexion {
+    public Connection conectar(){
+        String url = "jdbc:mysql://localhost:3306/formulario?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String user = "root";
+        String password = "";
+        Connection cn = null;
+        
+        try{
+            cn =DriverManager.getConnection(url,user, password);
+            if (cn !=null){
+                System.out.println("Conectado");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return cn;
+    }
+}
